7869154c98feccd32418dc84499ef84771e5638b
Jonan <jjsalomonzon@gmail.com>
2023-06-13 23:16:07 -0700

    added linter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import {useState} from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState } from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import NavBar from './components/NavBar';
 
-import HomePage from './pages/HomePage'
-
+import HomePage from './pages/HomePage';
 
 import { getUser } from './utilities/users-service';
 
 import './App.css';
 import Storybook from './pages/Storybook';
 
-
 function App() {
   const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
-     { user ? 
-      <>
-      <NavBar user={user} setUser={setUser}/>
-      <Routes>
-        <Route path='/' element={ <HomePage user={user} /> }/>
-      </Routes>
-      </>
-     : 
-      <AuthPage setUser={setUser}/>
-      }
-      <Storybook></Storybook>
+      {user ? (
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/" element={<HomePage user={user} />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
+      <Storybook />
     </main>
   );
 }
--- src/pages/Storybook.js
+++ src/pages/Storybook.js
@@ -1,30 +1,31 @@
-import { Canvas, useFrame } from "@react-three/fiber"
-import { useRef } from "react"
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/jsx-filename-extension */
+import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
 
 function Box() {
-    const myMesh = useRef()
+  const myMesh = useRef();
 
-    useFrame(({ clock }) => {
-        myMesh.current.rotation.x = clock.getElapsedTime()
-        myMesh.current.rotation.y = clock.getElapsedTime()
-        myMesh.current.rotation.z = clock.getElapsedTime()
-    })
+  useFrame(({ clock }) => {
+    myMesh.current.rotation.x = clock.getElapsedTime();
+    myMesh.current.rotation.y = clock.getElapsedTime();
+    myMesh.current.rotation.z = clock.getElapsedTime();
+  });
 
-    return(
-        <mesh ref={myMesh}>
-            <boxGeometry />
-            <meshStandardMaterial />
-        </mesh>
-    )
+  return (
+    <mesh ref={myMesh}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  );
 }
 
 export default function Storybook() {
-
-    return(
-        <Canvas>
-            <ambientLight intensity={0.1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
-            <Box/>
-        </Canvas>
-    )
+  return (
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <Box />
+    </Canvas>
+  );
 }
