cc68d42f5ac653c33f1a08f3b203ddd4d2fa3133
Jonan <jjsalomonzon@gmail.com>
2023-06-13 22:36:13 -0700

    added three scene

--- src/pages/Storybook.js
+++ src/pages/Storybook.js
@@ -1,9 +1,30 @@
-function Storybook(){
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
+function Box() {
+    const myMesh = useRef()
+
+    useFrame(({ clock }) => {
+        myMesh.current.rotation.x = clock.getElapsedTime()
+        myMesh.current.rotation.y = clock.getElapsedTime()
+        myMesh.current.rotation.z = clock.getElapsedTime()
+    })
+
     return(
-        <div>
-            <p>Storybook</p>
-        </div>
+        <mesh ref={myMesh}>
+            <boxGeometry />
+            <meshStandardMaterial />
+        </mesh>
     )
 }
 
-export default Storybook
+export default function Storybook() {
+
+    return(
+        <Canvas>
+            <ambientLight intensity={0.1} />
+            <directionalLight color="red" position={[0, 0, 5]} />
+            <Box/>
+        </Canvas>
+    )
+}
