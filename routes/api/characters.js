dc58ee254cc21144bd57bce5712ab17edf5f3856
Mayleen <mayleennyc@gmail.com>
2023-06-23 11:06:27 -0400

    updated routes with userId

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const charCtrl = require('../../controllers/api/characters')
 
+
+
+
+//Router endpoints defined utilizing the methods made from controller file (manipulating mongoDB)
+
 router.get('/all-char', charCtrl.allCharacters)
 router.get(':/id', charCtrl.getCharacter)
 router.post('/create-char', charCtrl.createCharacter)
--- src/utilities/formAPI.js
+++ src/utilities/formAPI.js
@@ -3,15 +3,33 @@ const BASE_URL = '/api/characters'
 //API functionalities
 //Create, Read, Update , Delete
 
-export function sendForm(charData){
-    return sendRequest(`${BASE_URL}/create`, 'POST', charData)
+export function sendForm(charData, userId){
+    return sendRequest(`${BASE_URL}/create-char/${userId}`, 'POST', charData)
+    //sending form to endpoint defined in routes/characters, creating the character
 }
 
 export function getChar(userId){
-    return sendRequest(`${BASE_URL}/`)
+    //make sure the userID is getting passed down from mongoDB so with each login, only the user gets their own characters 
+    return sendRequest(`${BASE_URL}/all-char/${userId}`, 'GET')
 }
 
+export function deleteChar(id, userId){
+    return sendRequest(`${BASE_URL}/all-char/${id}/${userId}`, 'DELETE')
+}
 
+export function editChar(id, userId, editData){
+    return sendRequest(`${BASE_URL}/edit-char/${id}/${userId}`, 'PUT', editData)
+}
+
+export function getCharById(id, userId){
+    return sendRequest(`${BASE_URL}/${id}/${userId}`)
+}
+
+// router.get('/all-char', charCtrl.allCharacters)
+// router.get(':/id', charCtrl.getCharacter)
+// router.post('/create-char', charCtrl.createCharacter)
+// router.delete('/all-char/:id', charCtrl.deleteCharacter)
+// router.put('/edit-char/:id', charCtrl.editCharacter)
 
 
 
@@ -22,6 +40,26 @@ export function getChar(userId){
 
 
 //--Helper Function
+function getToken() {
+    const token = localStorage.getItem('token');
+    
+    // if there is no token
+    console.log(token);
+    if (!token) return null;
+
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    // console.log(payload);
+
+    // if token is expired
+    if (payload.exp < Date.now() / 1000) {
+        localStorage.removeItem('token');
+        return null;
+    }
+
+    // token is valid
+    return token;
+
+}
 
 async function sendRequest(url, method = 'GET', payload = null) {
 
